7aa320c09ff35ddeb305be323fa7e849d6537c28
Declan Sweeney <declan.sweeney@gmail.com>
2025-03-29 14:03:37 +0000

    Add LoginUserRequest from https://youtu.be/TzAJfjCn7Ks?si=RIpTUrLPU0KST5Kv&t=2412

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginUserRequest;
 use App\Http\Requests\StoreUserRequest;
 use App\Traits\HttpResponses;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;  // Import the User modeluse Illuminate\Support\Facades\Validator;
@@ -12,32 +14,40 @@ class AuthController extends Controller
 {
     use HttpResponses;
 
-    public function login()
+    public function login(LoginUserRequest $request)
     {
-        return 'This is my login method';
+        $request->validated($request->all());
+
+        if (!Auth::attempt($request->only(['email', 'password']))) {
+        return $this->error('', 'Credentials do not match', 401);
+        }
+
+        $user = User::where('email', $request->email)->first();
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken('API Token of ' . $user->email . ' ' . $user->name)->plainTextToken
+        ]);
     }
 
 
-    public function register(Request $request)
+    public function register(StoreUserRequest $request)
     {
         // Validate the incoming request
-        $validated = $request->validate([
+        $validated = $request->validated([
             'name' => 'required|string|max:255',
             'email' => 'required|string|max:255|unique:users',
             'password' => 'required|confirmed|min:8', // 'confirmed' ensures password and password_confirmation match
         ]);
 
-        // Create the user if validation passes
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password), // Hash the password
         ]);
 
-        // Optionally, return a response or a token for the user
         return $this->success([
             'user' => $user,
-            'token' => $user->createToken('API Token of ' . $user->name)->plainTextToken
+            'token' => $user->createToken('API Token of ' . $user->email . ' ' . $user->name)->plainTextToken
         ]);
     }
 
--- app/Http/Requests/LoginUserRequest.php
+++ app/Http/Requests/LoginUserRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreUserRequest extends FormRequest
+class LoginUserRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -22,9 +22,8 @@ class StoreUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'max:255', 'unique:users'],
-            'password' => ['required', 'confirmed', 'min:8', 'regex:/[A-Za-z][0-9]/'],
+            'email' => ['required', 'string', 'email'],
+            'password' => ['required', 'min:8', 'regex:/[A-Za-z][0-9]/'],
         ];
     }
 }
